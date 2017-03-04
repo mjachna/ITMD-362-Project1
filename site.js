801db5965bdf22b5c5aeeafa87a691f521dd4d5d
mjachna <mjachna17@yahoo.com>
2017-03-03 22:25:38 -0600

    Added code to check for valid input in both fields before displaying submit button

--- site.js
+++ site.js
@@ -1,11 +1,24 @@
 $.noConflict();
-(function($) {
+
+jQuery(function($) {
   $(document).ready(
     function() {
+	  //determine if js is present
 	  $('html').removeClass('nojs');
-	  $('html').addClass('hasjs');
+      $('html').addClass('hasjs');	
+	    //listen for events on signup button
+	  $('#form-newsletter').on('keyup focus blur', function() {
+	      //get input values
+		var name = $('#name').val();
+        var email = $('#email').val();
+        var emailPattern = $('#email').attr('pattern');
+        var deals = $('#deals').val();
+        console.log('out');
+        if ((name !== ('')) && (email.match(emailPattern))) {
+		  console.log('in');
+          $('#submit').addClass('active');
+        } else {
+          $('#submit').removeClass('active');
+        }
 	}
-  )
-
-
-})
+)})});
