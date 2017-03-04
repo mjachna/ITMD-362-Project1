6ec6b966c4ca1db6e303db5bc07fbb7c76648b25
mjachna <mjachna17@yahoo.com>
2017-03-03 22:28:10 -0600

    Changed line endings and Tabs to spaces

--- site.js
+++ site.js
@@ -3,22 +3,22 @@ $.noConflict();
 jQuery(function($) {
   $(document).ready(
     function() {
-	  //determine if js is present
-	  $('html').removeClass('nojs');
-      $('html').addClass('hasjs');	
-	    //listen for events on signup button
-	  $('#form-newsletter').on('keyup focus blur', function() {
-	      //get input values
-		var name = $('#name').val();
+      //determine if js is present
+      $('html').removeClass('nojs');
+      $('html').addClass('hasjs');  
+        //listen for events on signup button
+      $('#form-newsletter').on('keyup focus blur', function() {
+          //get input values
+        var name = $('#name').val();
         var email = $('#email').val();
         var emailPattern = $('#email').attr('pattern');
         var deals = $('#deals').val();
         console.log('out');
         if ((name !== ('')) && (email.match(emailPattern))) {
-		  console.log('in');
+          console.log('in');
           $('#submit').addClass('active');
         } else {
           $('#submit').removeClass('active');
         }
-	}
+    }
 )})});
